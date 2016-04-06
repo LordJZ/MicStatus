0c9568738756006bd77d62fe33eb617c711674fd
LordJZ <LordJZ@outlook.com>
2016-04-06 05:29:03 +0300

    Added ToggleMic to toggle Mic status directly

---
+++ ToggleMic/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NAudio.CoreAudioApi;
+
+namespace ToggleMic
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            MMDevice audio = new MMDeviceEnumerator().GetDefaultAudioEndpoint(DataFlow.Capture, Role.Communications);
+            if (audio == null)
+                return;
+
+            AudioEndpointVolume volume = audio.AudioEndpointVolume;
+            volume.Mute = !volume.Mute;
+        }
+    }
+}
